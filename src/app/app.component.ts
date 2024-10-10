d82d0d9748fe773cfdd785fe86bb5e0282accadf
ramdanisalah67 <ramdanisalah67@gmail.com>
2024-10-10 15:08:06 +0100

    vf56d
    e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'product_test';
+export class AppComponent implements OnInit {
 
-
+  constructor(private meta: Meta, private title: Title) { }
+  
+  
   scrollToSection(section: any) {
     const element = document.getElementById(section);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
+
+  ngOnInit(): void {
+    this.title.setTitle('HBTP ');
+    this.meta.addTags([
+      { name: 'description', content: 'A description of your page for Google' },
+      { name: 'keywords', content: 'keywords, relevant, to, your, page' },
+      { name: 'robots', content: 'index, follow' }
+    ]);
+  }
+
+
 }
